003c3fdf03b219d6b65e6ba1ea2658cbd798eaae
kaidornel <kai_dornel@hotmail.com>
2020-10-07 21:21:36 -0400

    built migrations

--- db/migrate/20201008011608_books.rb
+++ db/migrate/20201008011608_books.rb
@@ -1,4 +1,11 @@
 class Books < ActiveRecord::Migration[6.0]
   def change
+    create_table :books do |t|
+      t.string :title
+      t.string :author
+      t.integer :pages
+
+      t.timestamps
+    end
   end
 end
--- db/migrate/20201008011620_reviews.rb
+++ db/migrate/20201008011620_reviews.rb
@@ -1,4 +1,10 @@
 class Reviews < ActiveRecord::Migration[6.0]
   def change
+    create_table :reviews do |t|
+      t.string :text
+      t.integer :stars
+
+      t.timestamps
+    end
   end
 end
