903057813c459da01b9c0f6d2aba273d58d81806
kaidornel <kai_dornel@hotmail.com>
2020-10-10 22:12:23 -0400

    build reviews controller methods

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,3 +1,34 @@
 class ReviewsController < ApplicationController
+    def index
+        @reviews = Review.all
+    end
 
+    def show
+        @review = Review.find(params[:id])
+    end
+
+    def new
+        @review = Review.new
+    end
+
+    def create
+        @review = Review.create(review_params)
+        redirect_to review_path(@review)
+    end
+
+    def edit
+        @review = Review.find(params[:id])
+    end
+
+    def update
+        @review = Review.find(params[:id])
+        @review.update(review_params)
+        redirect_to review_path(@review)
+    end
+
+    private
+
+    def review_params
+        params.require(:review).permit(:stars, :text)
+    end
 end
